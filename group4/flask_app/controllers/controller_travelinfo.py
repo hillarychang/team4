df0813344202ed9c9fd9105591bef33ec3c2f5fe
Hillary <hillarychang11@gmail.com>
2022-09-21 14:08:54 -0700

    fixed form

--- group4/flask_app/controllers/controller_travelinfo.py
+++ group4/flask_app/controllers/controller_travelinfo.py
@@ -11,33 +11,135 @@ from flask_app.models.user import User
 
 app.secret_key = "shhh"
 
+
 @app.route('/create_travelinfo', methods=["POST"])
 def create_travelinfo():
-    
     if not Travelinfo.validate_log(request.form): #request.form  (check user.py)
         return redirect('/create_travelinfo') 
 
-    
     data = {
             "text" : request.form["text"],
-            "user_id": session['user_id'],
-            "created_at": request.form["created_at"] #if we decide to make a created at in the log
+            "user_id": session['user_id']
+    
+    }
+    trip_id = Travelinfo.save(data)
+
+    flight_data = {
+            "destination" : request.form["destination"],
+            "trips_id": trip_id,
+            "departure" : request.form["departure"],
+            "arrival" : request.form["arrival"],
+            "flight_number" : request.form["flight_number"]
 
     }
-    id = Travelinfo.save(data)
-    return redirect('/showUser') 
 
+    flight_id = Travelinfo.saveFlight(flight_data)
+    
+    car_data = {
+            "company" : request.form["company"],
+            "trips_id": id,
+            "total_days" : request.form["total_days"],
+            "cost" : request.form["cost"],
+            "start_date" : request.form["start_date"],
+            "end_date" : request.form["end_date"]
+    
+    }
+    car_id = Travelinfo.saveCar(car_data)
+
+    hotel_data = {
+            "name" : request.form["name"],
+            "cost": request.form['cost'],
+            "trips_id": id,
+            "check_in" : request.form["check_in"],
+            "check_out" : request.form["check_out"]    
+    }
+    hotel_id = Travelinfo.saveHotel(hotel_data)
+    return redirect("/showUser") 
+
+
+
+
+# @app.route('/create_flights/<int:id>', methods=["POST"])
+# def create_flights(id):
+    
+#     data = {
+#             "destination" : request.form["destination"],
+#             "trips_id": id,
+#             "departure" : request.form["departure"],
+#             "arrival" : request.form["arrival"],
+#             "flight_number" : request.form["flight_number"]
+
+#     }
+#     id = Travelinfo.saveFlight(data)
+#     return redirect(f'/travelinfo/{id}') 
+
+
+# @app.route('/create_cars/<int:id>', methods=["POST"])
+# def create_cars(id):
+    
+#     data = {
+#             "company" : request.form["company"],
+#             "trips_id": id,
+#             "total_days" : request.form["total_days"],
+#             "cost" : request.form["cost"],
+#             "start_date" : request.form["start_date"],
+#             "end_date" : request.form["end_date"]
+
+    
+#     }
+#     id = Travelinfo.saveCar(data)
+#     return redirect(f'/travelinfo/{id}') 
+
+
+# @app.route('/create_hotels/<int:id>', methods=["POST"])
+# def create_hotels(id):
+    
+#     data = {
+#             "name" : request.form["name"],
+#             "cost": request.form['cost'],
+#             "trips_id": id,
+#             "check_in" : request.form["check_in"],
+#             "check_out" : request.form["check_out"]    
+#     }
+#     id = Travelinfo.saveHotel(data)
+#     return redirect(f'/travelinfo/{id}') 
+
+
+
+# @app.route('/create_travelinfo', methods=["POST"])
+# def create_travelinfo():
+    
+#     if not Travelinfo.validate_log(request.form): #request.form  (check user.py)
+#         return redirect('/create_travelinfo') 
+
+#     data = {
+#             "text" : request.form["text"],
+#             "user_id": session['user_id']
+    
+#     }
+#     id = Travelinfo.save(data)
+#     return redirect(f'/travelinfo/{id}') 
 
 
 @app.route("/travelinfo") #runs add_post.html
-def post():
-    
-    trips = Travelinfo.get_all()
-    data = {"id":session['user_id']} 
+def travelinfo():
+    return render_template("add_travelinfo.html") 
 
-    #ADDED
-    user = User.get_user_with_logs(data) #returns a user with a list of posts
-    return render_template("add_travelinfo.html", all_travelinfos = trips, users = user) 
+
+# @app.route("/travelinfo/<int:id>") #runs add_post.html
+# def travelinfo(id):
+    
+#     trips = Travelinfo.get_all()
+
+#     data = {'id':id}
+#     this_trip = Travelinfo.get_one(data)
+
+
+
+#     data = {"id":session['user_id']} 
+#     #ADDED
+#     user = User.get_user_with_logs(data) #returns a user with a list of posts
+#     return render_template("add_travelinfo.html", travelinfo = this_trip, all_travelinfos = trips, users = user) 
 
 
 
